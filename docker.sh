199ba369274cd3b4298ec430c8333e9f95ab588b
et0077 <32911115+et0077@users.noreply.github.com>
2017-10-21 17:55:12 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,3 +2,19 @@
 apt install docker.io --assume-yes
 apt install docker-compose --assume-yes
 /etc/init.d/docker start
+
+docker login --username=et007 --password=3Milian)
+
+docker network create demo-net
+
+ln -s /home/nimbus /home/et
+chown -R nimbus:nimbus /home/et
+
+ifconfig eth0:1 inet 10.0.0.5
+ifconfig eth0:2 inet 10.0.0.6
+ifconfig eth0:3 inet 10.0.0.7
+ifconfig eth0:4 inet 10.0.0.8
+ifconfig eth0:4 inet 10.0.0.9	
+ifconfig eth0:4 inet 10.0.0.10
+
+docker run -d --name proxy -p 80:80 --net demo-net admpresales/azure:nginx
