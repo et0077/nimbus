c2bc8e63240e23e95c1442fc1396f067fab726d7
et0077 <32911115+et0077@users.noreply.github.com>
2017-10-22 16:48:26 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -25,11 +25,11 @@ ifconfig eth0:4 inet 10.0.0.8
 ifconfig eth0:4 inet 10.0.0.9	
 ifconfig eth0:4 inet 10.0.0.10
 
-docker run -d --name devops -v /home/et/jobs:/var/lib/jenkins/jobs --hostname devops.aos.com --net demo-net admpresales/devops:1.1.2.0
+docker run -d --name devops -v /home/nimbus/jobs:/var/lib/jenkins/jobs --hostname devops.aos.com --net demo-net admpresales/devops:1.1.2.0
 docker run -d --hostname octane.aos.com --name octane --net demo-net --shm-size=2g admpresales/azure:octane
 #docker run -d --hostname mc.aos.com --name mc --net demo-net --shm-size=2g admpresales/azure:mc
 #docker run -d --name alm --hostname alm.aos.com --net demo-net --shm-size=2g admpresales/azure:alm
-docker run -d --name proxy -p 80:80 --net demo-net -v /home/et/proxy.conf:/etc/nginx/conf.d/default.conf admpresales/azure:nginx
+docker run -d --name proxy -p 80:80 --net demo-net -v /home/nimbus/proxy.conf:/etc/nginx/conf.d/default.conf admpresales/azure:nginx
 docker run -d --name aos_postgres --hostname aosdb.aos.com --net demo-net admpresales/aos-postgres:1.1.2
 
 ./launchAOS.sh
