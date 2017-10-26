47d1a924a01233486b2782cfaa3e8e6c70ed2dff
et0077 <32911115+et0077@users.noreply.github.com>
2017-10-26 09:17:13 +0200

    Update launchAOS.sh

--- launchAOS.sh
+++ launchAOS.sh
@@ -9,7 +9,7 @@ cat /home/nimbus/launchAOSnewtemp | sed  "s/westeurope/$DNS/g" >> /home/nimbus/l
 cat /home/nimbus/AOSWEBUndeploy.xml | sed  "s/aos.westeurope.cloudapp.azure.com/$AOS.$DNS.cloudapp.azure.com/g" > /home/nimbus/AOSWEBUndeployNew.xml
 cp /home/nimbus/AOSWEBUndeployNew.xml /home/nimbus/AOSWEBUndeploy.xml
 cat /home/nimbus/AOSWEBDeploy.xml | sed  "s/aos.westeurope.cloudapp.azure.com/$AOS.westeurope.cloudapp.azure.com/g" > /home/nimbus/AOSWEBDeployNew.xml
-cat /home/nimbus/AOSWEBDeployNew.xml | sed  "s/sis.westeurope.cloudapp.azure.com/$AOSA.westeurope.cloudapp.azure.com/g" > /home/nimbus/home/nimbus/AOSWEBDeploy.xml
+cat /home/nimbus/AOSWEBDeployNew.xml | sed  "s/sis.westeurope.cloudapp.azure.com/$AOSA.westeurope.cloudapp.azure.com/g" > /home/nimbus/AOSWEBDeploy.xml
 cat /home/nimbus/AOSWEBDeploy.xml | sed  "s/westeurope/$DNS/g" > /home/nimbus/AOSWEBDeployNew.xml
 cp /home/nimbus/AOSWEBDeployNew.xml /home/nimbus/AOSWEBDeploy.xml
 cat /home/nimbus/AOSOctane.xml | sed  "s/octane:8080/$Octane.$DNS.cloudapp.azure.com/g" > /home/nimbus/AOSOctaneNew.xml
