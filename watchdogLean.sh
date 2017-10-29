42360edc9040aae88840c0a3305d47177b18ca14
et0077 <32911115+et0077@users.noreply.github.com>
2017-10-29 17:27:41 +0100

    Update watchdogLean.sh

--- watchdogLean.sh
+++ watchdogLean.sh
@@ -1,13 +1,11 @@
 #/bin/bash
-NumberImages=$(cat /home/nimbus/ImagesNumber)
+CONTAINER_NAME='leanft'
 while true
 do
-        Number=$(docker ps |wc -l)
-        if [ $Number -ne $NumberImages ] ;
-        then
+        CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
+        if [ ! "${CID}" ]; then
+                docker rm -f leanft
                 docker run -d --name leanft -p 5900:5900 -e LFT_LIC_SERVER=autopass -e LFT_LIC_ID=23078 -e VERBOSE=true --net demo-net  functionaltesting/leanft-chrome:14.01
-                sleep 5
-        else
-                sleep 5
         fi
+        unset CID
 done
