ceb7b8f13e188b6cee9576f8e0d830d4af8e8b7f
et0077 <32911115+et0077@users.noreply.github.com>
2017-10-29 16:12:55 +0100

    Update dockeret.sh

--- dockeret.sh
+++ dockeret.sh
@@ -30,12 +30,12 @@ ifconfig eth0:7 inet 10.0.0.10
 
 ./changeProxy.sh
 
-docker run -d --name aos_postgres --hostname aosdb.aos.com --net demo-net admpresales/aos-postgres:1.1.2
+docker run -d --name aos_postgres --hostname aosdb.aos.com --net demo-net --restart=always admpresales/aos-postgres:1.1.2
 
 ./launchAOS.sh
 ./launchAOSnew2
 
-docker run -d --name devops -p 2022:22 --hostname devops.aos.com --ip=172.50.10.5 --net demo-net admpresales/devops:1.1.2.0
+docker run -d --name devops -p 2022:22 --hostname devops.aos.com --ip=172.50.10.5 --net demo-net --restart=always admpresales/devops:1.1.2.0
 docker exec devops useradd -m -p $(echo "nimbusPassw0rd123" | openssl passwd -1 -stdin) nimbus
 docker exec devops chown -R nimbus:nimbus /GitRepo
 docker stop devops
@@ -49,21 +49,21 @@ docker start devops
 if grep -q Only /home/nimbus/SIZE
 then
   docker run -d --hostname autopass.aos.com --ip=172.50.10.10 --name autopass --net demo-net --restart=always admpresales/autopass:9.3_v2
-  docker run -d --name leanft -p 5900:5900 -e LFT_LIC_SERVER=autopass -e LFT_LIC_ID=23078 -e VERBOSE=true --net demo-net  functionaltesting/leanft-chrome:14.01
-  docker run -d --name proxy -p 80:80 --net demo-net -v /home/nimbus/proxy.conf:/etc/nginx/conf.d/default.conf admpresales/azure:nginx
+  docker run -d --name leanft -p 5900:5900 -e LFT_LIC_SERVER=autopass -e LFT_LIC_ID=23078 -e VERBOSE=true --net demo-net --restart=always functionaltesting/leanft-chrome:14.01
+  docker run -d --name proxy -p 80:80 --net demo-net -v /home/nimbus/proxy.conf:/etc/nginx/conf.d/default.conf --restart=always admpresales/azure:nginx
 else
-docker run -d --hostname octane.aos.com --name octane --ip=172.50.10.6 --net demo-net --shm-size=2g admpresales/azure:octane
+docker run -d --hostname octane.aos.com --name octane --ip=172.50.10.6 --net demo-net --shm-size=2g --restart=always admpresales/azure:octane
 docker run -d --hostname autopass.aos.com --ip=172.50.10.10 --name autopass --net demo-net --restart=always admpresales/autopass:9.3_v2
-docker run -d --name leanft -p 5900:5900 -e LFT_LIC_SERVER=autopass -e LFT_LIC_ID=23078 -e VERBOSE=true --net demo-net  functionaltesting/leanft-chrome:14.01
-docker run -d --name proxy -p 80:80 --net demo-net -v /home/nimbus/proxy.conf:/etc/nginx/conf.d/default.conf admpresales/azure:nginx
+docker run -d --name leanft -p 5900:5900 -e LFT_LIC_SERVER=autopass -e LFT_LIC_ID=23078 -e VERBOSE=true --net demo-net --restart=always functionaltesting/leanft-chrome:14.01
+docker run -d --name proxy -p 80:80 --net demo-net -v /home/nimbus/proxy.conf:/etc/nginx/conf.d/default.conf --restart=always admpresales/azure:nginx
 if grep -q ALM /home/nimbus/SIZE
 then
-  docker run -d --hostname mc.aos.com --name mc --ip=172.50.10.9 --net demo-net --shm-size=2g admpresales/azure:mc
-  docker run -d --name alm --hostname alm.aos.com --ip=172.50.10.11 --net demo-net --shm-size=2g admpresales/azure:alm
+  docker run -d --hostname mc.aos.com --name mc --ip=172.50.10.9 --net demo-net --shm-size=2g --restart=always admpresales/azure:mc
+  docker run -d --name alm --hostname alm.aos.com --ip=172.50.10.11 --net demo-net --shm-size=2g --restart=always admpresales/azure:alm
 else
   if grep -q MC /home/nimbus/SIZE
     then
-      docker run -d --hostname mc.aos.com --name mc --ip=172.50.10.9 --net demo-net --shm-size=2g admpresales/azure:mc
+      docker run -d --hostname mc.aos.com --name mc --ip=172.50.10.9 --net demo-net --shm-size=2g --restart=always admpresales/azure:mc
   fi
 fi
 fi
