1c07617c42a326047776df6398f60c553f4be337
et0077 <32911115+et0077@users.noreply.github.com>
2017-10-30 09:42:01 +0100

    Update launchAOS.sh

--- launchAOS.sh
+++ launchAOS.sh
@@ -3,6 +3,7 @@ AOS=$(cat /home/nimbus/DNSAOS )
 AOSA=$(cat /home/nimbus/DNSAOSAccount )
 DNS=$(cat /home/nimbus/DNS )
 Octane=$(cat /home/nimbus/DNSOctane )
+Jenkins=$(cat /home/nimbus/DNSJenkins )
 cat /home/nimbus/launchAOS | sed  "s/nimbusaos/$AOS/g"  > /home/nimbus/launchAOSnew
 cat /home/nimbus/launchAOSnew | sed  "s/applicationaccount/$AOSA/g" >> /home/nimbus/launchAOSnewtemp
 cat /home/nimbus/launchAOSnewtemp | sed  "s/westeurope/$DNS/g" >> /home/nimbus/launchAOSnew2
@@ -14,3 +15,5 @@ cat /home/nimbus/AOSWEBDeploy.xml | sed  "s/westeurope/$DNS/g" > /home/nimbus/AO
 cp /home/nimbus/AOSWEBDeployNew.xml /home/nimbus/AOSWEBDeploy.xml
 cat /home/nimbus/AOSOctane.xml | sed  "s/octane:8080/$Octane.$DNS.cloudapp.azure.com/g" > /home/nimbus/AOSOctaneNew.xml
 cp /home/nimbus/AOSOctaneNew.xml /home/nimbus/AOSOctane.xml
+cat /home/nimbus/jenkins.model.JenkinsLocationConfiguration.xml | sed  "s/nimbusserver:8090/$Jenkins.$DNS.cloudapp.azure.com/g" > /home/nimbus/jenkconf.xml
+cp /home/nimbus/jenkconf.xml /home/nimbus/jenkins.model.JenkinsLocationConfiguration.xml
