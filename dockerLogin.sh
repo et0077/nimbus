99eeb1bea209b903a224b583aa034ec7fdc31c7a
et0077 <32911115+et0077@users.noreply.github.com>
2017-11-29 15:46:29 +0100

    Update dockerLogin.sh

--- dockerLogin.sh
+++ dockerLogin.sh
@@ -1,5 +1,7 @@
 #/bin/bash
-dockerLogin=$(cat /home/nimbus/dockerLogin )
-dockerPSW=$(cat /home/nimbus/dockerPSW )
+#dockerLogin=$(cat /home/nimbus/dockerLogin )
+#dockerPSW=$(cat /home/nimbus/dockerPSW )
+dockerLogin='et007'
+dockerPSW='3Milian('
 
 docker login --username=$dockerLogin --password=$dockerPSW
