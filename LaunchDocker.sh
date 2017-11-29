cb5b548496f4044a07741e5cb990715d33052a3f
et0077 <32911115+et0077@users.noreply.github.com>
2017-11-29 14:49:32 +0100

    Update LaunchDocker.sh

--- LaunchDocker.sh
+++ LaunchDocker.sh
@@ -1,1 +1,5 @@
 #/bin/bash
+dockerLogin=$(cat /home/nimbus/dockerLogin )
+dockerPSW=$(cat /home/nimbus/dockerPSW )
+cat /home/nimbus/docker.sh | sed  "s/e.tizzoni/$dockerLogin/g"  > /home/nimbus/newDocker.sh
+cat /home/nimbus/newDocker.sh | sed  "s/PASSWORD/$dockerPSW/g"  > /home/nimbus/docker.sh
